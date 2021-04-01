48fef594d20a9dc5a389c5de1bcd3f7175d2f4b8
tato <tato_fos@hotmail.com>
2021-04-01 14:39:30 -0400

    First Commit

---
+++ ConcurrentAplication/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConcurrentAplication
+{
+    class Program
+    {
+        public static Queue<int> genericQueue = new Queue<int>();
+        public static ConcurrentQueue<int> concurrentQueue = new ConcurrentQueue<int>();
+
+        public static void EnqueueNotConcurrent(){
+            for (int i = 0; i < 100; i++)
+            {
+                genericQueue.Enqueue(i);
+                Thread.Sleep(100);
+            }
+        }
+
+        public static void EnqueueConcurrent()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                concurrentQueue.Enqueue(i);
+                Thread.Sleep(100);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread threadNotConcurrent = new Thread(new ThreadStart(EnqueueNotConcurrent));
+            threadNotConcurrent.Start();
+            // Take out comment below to try genericQueuePeek
+            // Console.WriteLine("Generic Peek first " + genericQueue.Peek());
+
+            Thread threadConcurrent = new Thread(new ThreadStart(EnqueueConcurrent));
+            threadConcurrent.Start();
+            int result;
+            if (concurrentQueue.TryPeek(out result))
+            {
+                Console.WriteLine("Concurrent Peek first " + result);
+            }else
+            {
+                Console.WriteLine("Concurrent Not peeked ");
+            }
+        }
+    }
+}
